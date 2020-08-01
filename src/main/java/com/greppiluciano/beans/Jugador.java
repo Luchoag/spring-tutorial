bc2b4a9248a2d3eb4663dcc41597547f0c317744
Luciano Greppi <luchoag@gmail.com>
2020-08-01 10:54:41 -0300

    Tutorial 19 - Required Annotation

--- src/main/java/com/greppiluciano/beans/Jugador.java
+++ src/main/java/com/greppiluciano/beans/Jugador.java
@@ -1,5 +1,7 @@
 package com.greppiluciano.beans;
 
+import org.springframework.beans.factory.annotation.Required;
+
 import com.greppiluciano.interfaces.IEquipo;
 
 public class Jugador {
@@ -24,9 +26,17 @@ public class Jugador {
 	public IEquipo getEquipo() {
 		return equipo;
 	}
+	
+	@Required
 	public void setEquipo(IEquipo equipo) {
 		this.equipo = equipo;
 	}
+	
+	/*
+	 * Al incluir la anotación Required y el bean correspondiente en beans.xml, si no se completa el campo requerido, el programa arroja una excepción
+	 * en la que indica cuál es el error cometido. Si no se agrega el bean RequiredAnnotationBeanPostProcessor, también arroja excepción pero al no
+	 * reconocer la anotación, no especifica cuál ha sido el error.
+	 */
 	
 	
 }
