b421063b3df7570986025f985aefc1e443854a64
Luciano Greppi <luchoag@gmail.com>
2020-08-01 08:53:19 -0300

    Tutorial 12 - Scope de los Beans (Singleton y Prototype)

--- src/main/java/com/greppiluciano/spring/App.java
+++ src/main/java/com/greppiluciano/spring/App.java
@@ -22,8 +22,25 @@ public class App {
 		//appContext.refresh();
 		
 		Persona per = (Persona) appContext.getBean("persona");
+		Persona per2 = (Persona) appContext.getBean("persona");
+		
+		/*
+		 * Con Singleton en el scope del Bean, estas propiedades se asignan a las dos instancias, ya que en
+		 * realidad es una sola.
+		 * Con Prototype en el scope del Bean, se asignan sólo a la instancia referenciada. 
+		 */
+		per.setId(1);
+		per.setNombre("Luciano");
+		per.setApodo("Lucho");		
 		
 		System.out.println(per.getId() + " " + per.getNombre() + " " + per.getApodo() + " " + per.getPais().getNombre() + " " + per.getCiudad().getNombre());
+		System.out.println(per2.getId() + " " + per2.getNombre() + " " + per2.getApodo() + " " + per2.getPais().getNombre() + " " + per2.getCiudad().getNombre());
+		
+		// Con Singleton en el "Scope" del Bean, los objetos tienen el mismo id
+		// Con Prototype en el "Scope" del Bean, los objetos tienen distinto id
+		//System.out.println(per);
+		//System.out.println(per2);		
+		
 		
 		// Cierra el appContext
 		((ConfigurableApplicationContext)appContext).close();
